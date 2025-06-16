9327e512bc5f6eae55594067a0991890e0cd8a08
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-06-16 23:30:26 +0530

    🔹 Day 1: Revenue analysis by country using SUM, CASE, and GROUP BY

--- Day01/notes.md
+++ Day01/notes.md
@@ -1,2 +1,2 @@
-## Day 1 - SELECT Basics
+## Day 1 - Day 1: Revenue analysis by country using SUM, CASE, and GROUP BY
 Practiced SELECT queries and fetched rows from tables.
--- Day01/query.sql
+++ Day01/query.sql
@@ -3,13 +3,11 @@
 SELECT 
     Country,
     ROUND(SUM(Quantity * UnitPrice), 2) AS TotalRevenue,
-    
     CASE
-        WHEN SUM(Quantity * UnitPrice) > 500000 THEN '🔥 High Value Market'
-        WHEN SUM(Quantity * UnitPrice) BETWEEN 100000 AND 500000 THEN '🌟 Mid Tier Market'
-        ELSE '📉 Low Tier Market'
+        WHEN SUM(Quantity * UnitPrice) > 500000 THEN 'High Value Market'
+        WHEN SUM(Quantity * UnitPrice) BETWEEN 100000 AND 500000 THEN 'Mid Tier Market'
+        ELSE 'Low Tier Market'
     END AS RevenueCategory
-
 FROM Orders
 WHERE Quantity > 0 AND UnitPrice > 0
 GROUP BY Country
