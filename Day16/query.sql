7a7007f8abc4d4f17bfe1bbdc6429e88ba66d87b
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-07-01 13:39:45 +0530

    Add Day 16 SQL: customers with highest total order value

---
+++ Day16/query.sql
@@ -0,0 +1,8 @@
+## Day 16 – Top Spending Customers
+
+**Challenge:** Find customers who spent the most overall.
+
+**Learned:**
+- Aggregation using `SUM()`
+- Grouping and sorting with `GROUP BY` and `ORDER BY`
+- Insights into customer value
