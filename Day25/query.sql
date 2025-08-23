8c6e006cf8205e0afa20344931bb7f039c6f1419
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-08-23 20:11:59 +0530

    Add Day 25 SQL: top 10 customers by spending

---
+++ Day25/query.sql
@@ -0,0 +1,9 @@
+-- Day 24 : Find top 10 customers by total spending
+
+SELECT c.customer_name, SUM(oi.quantity * oi.unit_price) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+GROUP BY c.customer_name
+ORDER BY total_spent DESC
+LIMIT 10;
